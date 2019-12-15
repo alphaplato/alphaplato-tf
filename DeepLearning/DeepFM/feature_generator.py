9a7935f6f6d5412a8b1e9cbbbed28fc3cc651b44
alphaplato <plato.sg.lee@gmail.com>
2019-12-15 23:54:09 +0800

    update deepfm

---
+++ DeepLearning/DeepFM/feature_generator.py
@@ -0,0 +1,72 @@
+#!/bin/python3
+#coding:utf-8
+#Copyright 2019 Alphaplato. All Rights Reserved.
+#Desc:read tfrecord and generate feature columns
+#=======================================================
+import json
+import multiprocessing
+import tensorflow as tf
+
+
+def feature_json(fea_json_path):
+    with open(fea_json_path,'r') as fr:
+        fea_json = json.load(fr)
+    return fea_json
+
+class FeatureGenerator(object):
+    def __init__(self,feature_json):
+        self._feature_json = feature_json
+        self._feature_generate()
+
+    def _feature_generate(self):
+        feature_columns = {}
+        #deepfm lr feature columns process
+        lr_feature_columns = {}
+        for fea in self._feature_json['features']:
+            if fea['feature_type'] == 'raw':
+                lr_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
+            elif fea['feature_type'] == 'id':
+                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
+                lr_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=1)
+        feature_columns['lr'] = lr_feature_columns
+        #deepfm fm feature columns process
+        fm_feature_columns = {}
+        for fea in self._feature_json['features']:
+            if fea['feature_type'] == 'raw':
+                fm_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
+            elif fea['feature_type'] == 'id':
+                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
+                fm_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=fea['embedding'])
+            raw_fields = [fea['feature_name'] for fea in self._feature_json['features']]
+            x_feature = tf.feature_column.categorical_column_with_vocabulary_list('raw_fields',raw_fields)
+            fm_feature_columns['raw_fields'] = tf.feature_column.embedding_column(x_feature,dimension=16)
+          
+        feature_columns['fm'] = fm_feature_columns       
+        #deepfm deep feature columns process
+        deep_feature_columns = {}
+        for fea in self._feature_json['features']:
+            if fea['feature_type'] == 'raw':
+                deep_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
+            elif fea['feature_type'] == 'id':
+                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
+                deep_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=fea['embedding'])
+        feature_columns['deep'] = deep_feature_columns
+        self.feature_columns = feature_columns
+
+    def _parser(self,record):
+        fea_dict = {}
+        for fea in self._feature_json['features']:
+            if fea['value_type'] == 'Double':
+                fea_dict[fea['feature_name']] = tf.FixedLenFeature(shape=[1],dtype=tf.float32)
+            elif fea['value_type'] == 'String':
+                fea_dict[fea['feature_name']] = tf.VarLenFeature(tf.string)
+        fea_dict['label'] = tf.FixedLenFeature(shape=[1],dtype=tf.int64)
+        features = tf.parse_single_example(record, fea_dict)
+        label =  features.pop('label')
+        return features,label
+
+    def input_fn(self,data_path,mode=tf.estimator.ModeKeys.TRAIN,batch_size=1,num_epochs=1):
+        dataset = tf.data.TFRecordDataset(data_path,num_parallel_reads=4)
+        dataset = dataset.map(self._parser).repeat(num_epochs).batch(batch_size)
+        features,label = dataset.make_one_shot_iterator().get_next()
+        return features,label
---
+++ DeepLearning/DeepFM/train.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# coding=utf-8
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+
+import shutil
+import os
+import json
+import tensorflow as tf
+
+from feature_json import feature_json
+from feature_generator import FeatureGenerator
+from deep_fm import Model
+
+#################### CMD Arguments ####################
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_integer("dist_mode", 0, "distribuion mode {0-loacal, 1-single_dist, 2-multi_dist}")
+tf.app.flags.DEFINE_string("ps_hosts", '', "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("worker_hosts", '', "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("job_name", '', "One of 'ps', 'worker'")
+tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+tf.app.flags.DEFINE_integer("num_threads", 16, "Number of threads")
+tf.app.flags.DEFINE_integer("embedding_size", 32, "Embedding size")
+tf.app.flags.DEFINE_integer("num_epochs", 10, "Number of epochs")
+tf.app.flags.DEFINE_integer("batch_size", 64, "Number of batch size")
+tf.app.flags.DEFINE_integer("log_steps", 1000, "save summary every steps")
+tf.app.flags.DEFINE_float("learning_rate", 0.0005, "learning rate")
+tf.app.flags.DEFINE_float("l2_reg", 0.0001, "L2 regularization")
+tf.app.flags.DEFINE_string("optimizer", 'Adam', "optimizer type {Adam, Adagrad, GD, Momentum}")
+tf.app.flags.DEFINE_string("deep_layers", '256,128,64', "deep layers")
+tf.app.flags.DEFINE_string("dropout", '0.5,0.5,0.5', "dropout rate")
+tf.app.flags.DEFINE_boolean("batch_norm", False, "perform batch normaization (True or False)")
+tf.app.flags.DEFINE_float("batch_norm_decay", 0.9, "decay for the moving average(recommend trying decay=0.9)")
+tf.app.flags.DEFINE_string("data_dir", '', "data dir")
+tf.app.flags.DEFINE_string("dt_dir", '', "data dt partition")
+tf.app.flags.DEFINE_string("model_dir", '', "model check point dir")
+tf.app.flags.DEFINE_string("servable_model_dir", '', "export servable model for TensorFlow Serving")
+tf.app.flags.DEFINE_string("task_type", 'train', "task type {train, infer, eval, export}")
+tf.app.flags.DEFINE_boolean("clear_existing_model", False, "clear existing model or not")
+
+
+def set_dist_env():
+    if FLAGS.dist_mode == 1:  # 本地分布式测试模式1 chief, 1 ps, 1 evaluator
+        ps_hosts = FLAGS.ps_hosts.split(',')
+        chief_hosts = FLAGS.chief_hosts.split(',')
+        task_index = FLAGS.task_index
+        job_name = FLAGS.job_name
+        print('ps_host', ps_hosts)
+        print('chief_hosts', chief_hosts)
+        print('job_name', job_name)
+        print('task_index', str(task_index))
+        # 无worker参数
+        tf_config = {
+            'cluster': {'chief': chief_hosts, 'ps': ps_hosts},
+            'task': {'type': job_name, 'index': task_index}
+        }
+        print(json.dumps(tf_config))
+        os.environ['TF_CONFIG'] = json.dumps(tf_config)
+    elif FLAGS.dist_mode == 2:  # 集群分布式模式
+        ps_hosts = FLAGS.ps_hosts.split(',')
+        worker_hosts = FLAGS.worker_hosts.split(',')
+        chief_hosts = worker_hosts[0:1]  # get first worker as chief
+        worker_hosts = worker_hosts[2:]  # the rest as worker
+        task_index = FLAGS.task_index
+        job_name = FLAGS.job_name
+        print('ps_host', ps_hosts)
+        print('worker_host', worker_hosts)
+        print('chief_hosts', chief_hosts)
+        print('job_name', job_name)
+        print('task_index', str(task_index))
+        # use #worker=0 as chief
+        if job_name == "worker" and task_index == 0:
+            job_name = "chief"
+        # use #worker=1 as evaluator
+        if job_name == "worker" and task_index == 1:
+            job_name = 'evaluator'
+            task_index = 0
+        # the others as worker
+        if job_name == "worker" and task_index > 1:
+            task_index -= 2
+
+        tf_config = {
+            'cluster': {'chief': chief_hosts, 'worker': worker_hosts, 'ps': ps_hosts},
+            'task': {'type': job_name, 'index': task_index}
+        }
+        print(json.dumps(tf_config))
+        os.environ['TF_CONFIG'] = json.dumps(tf_config)
+
+def main(_):
+    tf.logging.set_verbosity(tf.logging.INFO)
+    # set_dist_env()
+    #------bulid Tasks------
+    model_params = {
+        "learning_rate": FLAGS.learning_rate,
+        "batch_norm_decay": FLAGS.batch_norm_decay,
+        "l2_reg": FLAGS.l2_reg,
+        "deep_layers": FLAGS.deep_layers,
+        "dropout": FLAGS.dropout,
+        "optimizer":FLAGS.optimizer
+    }
+
+    if FLAGS.clear_existing_model:
+        try:
+            shutil.rmtree(FLAGS.model_dir)
+        except Exception as e:
+            print(e, "at clear_existing_model")
+        else:
+            print("existing model cleaned at %s" % FLAGS.model_dir)    
+
+    tr_files='./csv.tfrecords'
+    va_files='./csv2.tfrecords'
+
+
+    fea_json = feature_json('./feature_generator.json')
+    fg = FeatureGenerator(fea_json)
+    model = Model(fg)
+
+    config = tf.estimator.RunConfig().replace(session_config = tf.ConfigProto(device_count={'GPU':0, 'CPU':FLAGS.num_threads}),
+            log_step_count_steps=1000, save_summary_steps=1000)
+    DeepFM = tf.estimator.Estimator(model_fn=model.model_fn, model_dir='./model/', params=model_params, config=config)
+
+    tr_files = "./data/train.tfrecords"
+    va_files ="./data/test.tfrecords"
+
+    if FLAGS.task_type == 'train':
+        train_spec = tf.estimator.TrainSpec(input_fn=lambda: model.fg.input_fn(tr_files, num_epochs=FLAGS.num_epochs,batch_size=FLAGS.batch_size))
+        eval_spec = tf.estimator.EvalSpec(input_fn=lambda: model.fg.input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), steps=None, start_delay_secs=1000, throttle_secs=1200)
+        tf.estimator.train_and_evaluate(DeepFM, train_spec, eval_spec)
+    elif FLAGS.task_type == 'eval':
+        DeepFM.evaluate(input_fn=lambda: model.fg.input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size))
+    elif FLAGS.task_type == 'infer':
+        preds = DeepFM.predict(input_fn=lambda: model.fg.input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), predict_keys="prob")
+        with open(FLAGS.data_dir + "./data/pred.txt", "w") as fo:
+            for prob in preds:
+                fo.write("%f\n" % (prob['prob']))
+
+if __name__=='__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
