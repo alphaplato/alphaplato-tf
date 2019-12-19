d3f039d1f218fa77fc08e6b2f6d6cd8a80b6dda6
alphaplato <plato.sg.lee@gmail.com>
2019-12-19 13:04:03 +0800

    fix bug

--- DeepLearning/DCN/deep_dcn.py
+++ DeepLearning/DCN/deep_dcn.py
@@ -28,22 +28,21 @@ class DCN(object):
                         dnn_input = tf.nn.dropout(dnn_input,dropout[i])
                     else:
                         dnn_input = tf.layers.batch_normalization(dnn_input,training = False)
-                dnn_out = tf.layers.dense(dnn_input,1) # 注意：输出层使用线性激活函数！！！！
 
             with tf.variable_scope("dcn_net"):
-                xl = dnn_input
+                xl = deep_input
                 embed_dim = xl.get_shape().as_list()[1]
                 for i in range(dcn_layers):
                     wl = tf.get_variable(name= "cross_w_{0}".format(i),shape=[embed_dim],regularizer=tf.contrib.layers.l2_regularizer(l2_reg),initializer=tf.truncated_normal_initializer(stddev=0.01))
                     bl = tf.get_variable(name= "cross_b_{0}".format(i),shape=[embed_dim],regularizer=tf.contrib.layers.l2_regularizer(l2_reg),initializer=tf.truncated_normal_initializer(stddev=0.01))
                     xlw = tf.matmul(xl,tf.reshape(wl,shape=[-1,1]))
-                    xl = tf.multiply(dnn_input,xlw) + bl + xl
-                if mode == tf.estimator.ModeKeys.TRAIN:
-                    xl = tf.layers.batch_normalization(xl,training = True)
-                else:
-                    xl = tf.layers.batch_normalization(xl,training = False)
+                    xl = tf.multiply(deep_input,xlw) + bl + xl
+                    if mode == tf.estimator.ModeKeys.TRAIN:
+                        xl = tf.layers.batch_normalization(xl,training = True)
+                    else:
+                        xl = tf.layers.batch_normalization(xl,training = False)
             
-            y_input = tf.concat([dnn_out,xl],axis=1)
+            y_input = tf.concat([dnn_input,xl],axis=1)
             y_out = tf.layers.dense(xl,1)
 
             return  y_out
