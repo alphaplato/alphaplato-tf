03e5684f3d0aabb2130136cc13277fcf42e42887
alphaplato <plato.sg.lee@gmail.com>
2020-05-08 18:47:40 +0800

    del useless file feature_json.py

--- DeepLearning/XDFM/feature_json.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import tensorflow as tf
-import json
-
-def feature_json(fea_json_path):
-    with open(fea_json_path,'r') as fr:
-        fea_json = json.load(fr)
-    return fea_json
