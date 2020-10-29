2ac2aaf6cbfcfd20b0f2fe42845cc52fe5611e5e
alphaplato <plato.sg.lee@gmail.com>
2020-10-29 18:18:17 +0800

    update & fix bug in fg.py

--- DeepLearning/DCN/feature_generator.py
+++ DeepLearning/DCN/feature_generator.py
@@ -13,38 +13,13 @@ class FeatureGenerator(object):
         self._feature_holder()
 
     def _feature_generate(self):
-        feature_columns = {}
-        #deepfm lr feature columns process
-        lr_feature_columns = {}
+        feature_columns = []
         for fea in self._feature_json['features']:
             if fea['feature_type'] == 'raw':
-                lr_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
+               feature_columns.append(tf.feature_column.numeric_column(fea['feature_name']))
             elif fea['feature_type'] == 'id':
-                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
-                lr_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=1)
-        feature_columns['lr'] = lr_feature_columns
-        #deepfm fm feature columns process
-        fm_feature_columns = {}
-        for fea in self._feature_json['features']:
-            if fea['feature_type'] == 'raw':
-                fm_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
-            elif fea['feature_type'] == 'id':
-                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
-                fm_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=fea['embedding'])
-            raw_fields = [fea['feature_name'] for fea in self._feature_json['features']]
-            x_feature = tf.feature_column.categorical_column_with_vocabulary_list('raw_fields',raw_fields)
-            fm_feature_columns['raw_fields'] = tf.feature_column.embedding_column(x_feature,dimension=16)
-          
-        feature_columns['fm'] = fm_feature_columns       
-        #deepfm deep feature columns process
-        deep_feature_columns = {}
-        for fea in self._feature_json['features']:
-            if fea['feature_type'] == 'raw':
-                deep_feature_columns[fea['feature_name']] = tf.feature_column.numeric_column(fea['feature_name'])
-            elif fea['feature_type'] == 'id':
-                x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
-                deep_feature_columns[fea['feature_name']] = tf.feature_column.embedding_column(x_feature,dimension=fea['embedding'])
-        feature_columns['deep'] = deep_feature_columns
+               x_feature = tf.feature_column.categorical_column_with_hash_bucket(fea['feature_name'],hash_bucket_size=fea['hash_size'])
+               feature_columns.append(tf.feature_column.embedding_column(x_feature,dimension=fea['embedding']))
         self.feature_columns = feature_columns
 
     def _feature_specify(self):
